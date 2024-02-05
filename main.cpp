c231495656d737a5dface90f6b8019a42749545e
Valtteri K.A.P <144036143+Dukk991@users.noreply.github.com>
2024-02-05 19:32:48 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "asunto.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    Asunto olio_1;
+
+    olio_1.maarita(2, 100);
+    olio_1.laskeKulutus(200);
+
+    return 0;
+}
