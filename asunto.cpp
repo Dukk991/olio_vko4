9107003f796502bb7ea0fd0455b6bce7454528e3
Valtteri K.A.P <144036143+Dukk991@users.noreply.github.com>
2024-02-06 16:47:25 +0200

    Update asunto.cpp

--- asunto.cpp
+++ asunto.cpp
@@ -8,13 +8,12 @@ Asunto::Asunto()
     cout << "Asunto luotu" << endl;
 }
 
-void Asunto::maarita(int asukkaat, int neliot)
+void Asunto::maarita(int asukasMaara, int neliot)
 {
-    cout << "Asunto maaritetty, asukkaita = " << asukkaat << " , nelioita = " << neliot << endl;
+    cout << "Asunto maaritetty, asukkaita = " << asukasMaara << " , nelioita = " << neliot << endl;
 }
 
 double Asunto::laskeKulutus(double hinta)
 {
-    cout << "Asunnon kulutus, kun hinta on = 1 on " << hinta << endl;
-    return hinta;
+    return hinta * 200;
 }
