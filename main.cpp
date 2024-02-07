8d528747ecdfccfdb78f17751eb971a914963189
Valtteri K.A.P <144036143+Dukk991@users.noreply.github.com>
2024-02-07 16:49:37 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,11 @@
-#include "asunto.h"
+#include "kerrostalo.h"
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    Asunto asunto;
-
-    asunto.maarita(2, 100);
-    double hinta = 1;
-    cout << "Asunnon kulutus kun hinta = " << hinta << ", on = " << asunto.laskeKulutus(hinta) << endl;
+    Kerrostalo kerrostalo;
+    kerrostalo.laskeKulutus(1);
+    return 0;
 }
